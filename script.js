a40f844559a1c42023d8712b3d15b6576edb2659
Zagham Arshad <zaghambajwa57@gmail.com>
2023-08-20 15:41:55 -0600

    Learning

---
+++ script.js
@@ -0,0 +1,13 @@
+ // Purpose: To display the current date and time
+ 
+
+ const displayDate =() => {
+
+   return document.getElementById("div1").style.color = "white";
+ }
+
+ function displayNormal() {
+    //convert time to local time
+
+    document.getElementById("div1").style.color = "white";
+  }
